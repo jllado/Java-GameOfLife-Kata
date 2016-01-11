a3a3f0897d8410a500750b936d208abf87a76e89
Juan Llado <jllado@gmail.com>
2016-01-11 19:18:24 +0100

    Initial test TODO.

--- src/test/java/katas/gameoflife/GameTest.java
+++ src/test/java/katas/gameoflife/GameTest.java
@@ -1,4 +1,4 @@
-package katas;
+package katas.gameoflife;
 
 /**
 - Generate a grid with random cells
@@ -6,6 +6,15 @@ package katas;
 - 2. Any live cell with more than three live neighbours dies, as if by overcrowding.
 - 3. Any live cell with two or three live neighbours lives on to the next generation.
 - 4. Any dead cell with exactly three live neighbours becomes a live cell.
+ */
+/*
+- Grid 1x1 with one live cell
+- Grid 1x1 with zero live cell
+- Grid 1x1 with one live cell
+- Grid 1x2 with one live cell
+- Grid 1x2 with two live cell
+- Given Grid 1x2 with one live cell, When iterate, Then zero live zells
+- Given Grid 1x3 with two separte live cells, When iterate, Then zero live zells
  */
 public class GameTest {
 
